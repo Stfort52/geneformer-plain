25b50122ca6509774cb773c8a5b18f8c8761e8dd
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2025-03-05 20:45:11 +0900

    fix: typo on file name

--- masters/model/utils/__init__.py
+++ masters/model/utils/__init__.py
@@ -1,6 +1,6 @@
 from .activation import activation_from_name as activation_from_name
 from .callback import EvenlySpacedModelCheckpoint as EvenlySpacedModelCheckpoint
-from .initalization import reset_weights as reset_weights
+from .initialization import reset_weights as reset_weights
 from .metrics import continuous_metrics as continuous_metrics
 from .metrics import threshold_metrics as threshold_metrics
 from .pe_from_name import pe_from_name as pe_from_name
